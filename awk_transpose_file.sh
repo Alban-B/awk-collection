b7f65478f41f15f9219867368fe39f578b3cce24
Alban-B <Alban-B@users.noreply.github.com>
2015-11-05 14:08:50 +0100

    Create awk_transpose_file.sh

---
+++ awk_transpose_file.sh
@@ -0,0 +1,18 @@
+## ce script bash permet de transposer un fichier (i.e les colonnes seront alors les lignes)
+#!/bin/sh
+awk '
+{ 
+    for (i=1; i<=NF; i++)  {
+        a[NR,i] = $i
+    }
+}
+NF>p { p = NF }
+END {    
+    for(j=1; j<=p; j++) {
+        str=a[1,j]
+        for(i=2; i<=NR; i++){
+            str=str" "a[i,j];
+        }
+        print str
+    }
+}' 
